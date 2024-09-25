468858bf6139baf30d815d6327a6b3eafec855c5
amybradbrook <amy.bradbrook@live.ca>
2024-09-24 21:09:46 -0400

    Added firebase features'

--- app/src/main/java/com/example/roommatechores/MainActivity.java
+++ app/src/main/java/com/example/roommatechores/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.roommatechores;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,5 +32,34 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         mAuth = FirebaseAuth.getInstance();
+    }
+
+
+    public void onClick(View view){
+
+        TextView emailTextView = findViewById(R.id.emailInput);
+        TextView passwordView = findViewById(R.id.passwordInput);
+        String email, password;
+        email = emailTextView.getText().toString();
+        password = passwordView.getText().toString();
+        logIn(email, password);
+    }
+
+    private void logIn(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(),
+                                    "Login Successful!",
+                                    Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Login failed!", Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                }
+        );
     }
 }
